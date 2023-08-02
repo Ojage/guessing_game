e5ed1005d498eb9a66db30bccc3968c50bc57637
Ojage <hismajestylordkratos@gmail.com>
2023-08-03 00:28:19 +0100

    Cantake input

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use rand::Rng;
+use std::io; //we need to bring the io input/output library into scope.
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess: ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {}", guess);
+}
